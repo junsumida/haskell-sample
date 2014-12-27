be15731aa3cf9cb0f53a7e89eb489f2931c9fdc0
Jun Sumida <sekitoh.rh@gmail.com>
2014-12-28 03:31:25 +0900

    Now first Scotty works

--- Main.hs
+++ Main.hs
@@ -1,6 +1,30 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Web.Scotty
+import Web.Scotty as WSC
+import Data.Monoid
+import Data.Aeson
+import Data.Map
+
+data Person = Person {
+    name :: String,
+    job  :: String
+  } deriving Show
+
+instance ToJSON Person where
+  toJSON (Person name job) =
+    object [ "name" .= name, "job" .= job]
 
 main = scotty 3000 $ do
   get "/" $ do
     html "Hello World!"
+
+  get "/me" $ do
+    WSC.json (Person "junsumida" "software engineer")
+
+  get "/word/:word" $ do
+    beam <- param "word"
+    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+
+  -- 404
+  notFound $ do
+    text "there is no such route."
+
